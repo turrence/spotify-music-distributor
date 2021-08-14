fb69786189e88e9dc17e2c8aa5b854e887f44d97
Terence Tong <tjtong2006@gmail.com>
2021-08-13 21:19:15 -0700

    front end buttons can now do a post request

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
 
 class Source_Destinations(BaseModel):
-<<<<<<< HEAD
     source: str
     destinations: list
+<<<<<<< HEAD
 =======
-    source_playlist: str
-    destination_playlists: list
->>>>>>> 74e035f... front end buttons can now do a post request
+
+>>>>>>> aee0424... fixup
--- frontend/music-distributor/src/Components/Buttons/SendPlaylistsButton.js
+++ frontend/music-distributor/src/Components/Buttons/SendPlaylistsButton.js
@@ -7,10 +7,6 @@ function SendPlaylistsButton( { srcPlaylist, destPlaylists }){
     function sendOnClick(){
         const data = {"source" : srcPlaylist, "destinations" : destPlaylists}
         send_source_destination_playlists(data)
-<<<<<<< HEAD
-=======
-        document.write("hi")
->>>>>>> 74e035f... front end buttons can now do a post request
     }
 
     return (<Button disabled={srcPlaylist === "" || destPlaylists.length === 0} 
